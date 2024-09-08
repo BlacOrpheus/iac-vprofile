9433507ade8c3fc8b5e2f31021b6215cfefc0ddf
gitopsproject <chidubemchinwuba01@gmail.com>
2024-09-08 11:21:19 +0100

    latest

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofiles3bucket007"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
