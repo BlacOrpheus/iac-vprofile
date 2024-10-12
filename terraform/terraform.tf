2c10b80468a80acdaf5389e00210e79c2f06c48e
BlacOrpheus <chidubemchinwuba01@gmail.com>
2024-10-12 12:52:50 +0100

    yes yes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofiles3bucket007"
     key    = "terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
