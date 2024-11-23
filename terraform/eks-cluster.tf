bd010ca5b91662f3a671de1b55bd23c73d996c88
BlacOrpheus <chidubemchinwuba01@gmail.com>
2024-11-23 21:39:27 +0100

    hwbewj

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.29.0"
+  version = "19.19.1"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
@@ -11,24 +11,32 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
+
   }
 
   eks_managed_node_groups = {
     one = {
-      name            = "node-group-1"
-      instance_types  = ["t3.small"]
-      min_size        = 1
-      max_size        = 3
-      desired_size    = 2
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
 
     two = {
-      name            = "node-group-2"
-      instance_types  = ["t3.small"]
-      min_size        = 1
-      max_size        = 2
-      desired_size    = 1
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
   }
 
+  create_kms_key   = false   # Set this variable to false to prevent KMS key creation
 }
+
+###
